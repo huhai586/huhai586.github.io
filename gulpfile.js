5f25322d3dd3b1c49b698c1f0935d93d1148961e
huhai <huhai007@gmail.com>
2017-07-02 20:34:09 +0800

    ackup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp         = require('gulp'),
   plumber      = require('gulp-plumber'), // Prevent pipe breaking caused by errors from gulp plugins
-  jade         = require('gulp-jade');
+  jade         = require('gulp-jade'),
+  sass         = require('gulp-sass');
+
 
   gulp.task("jade", function () {
   var feeds = ['./template/index.jade'];
-
   return gulp.src(feeds)
     .pipe(plumber())
     .pipe(jade({ pretty: true }))
     .pipe(gulp.dest('./'))
+});
+
+  gulp.task('sass', function () {
+  return gulp.src('./assets/css/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("./assets/css"));
 });
