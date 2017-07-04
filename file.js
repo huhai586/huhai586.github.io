83ef3c066665a688f30cf573e09e306774e18ae5
胡海 <huhai@huhaideMacBook-Pro.local>
2017-07-04 16:58:46 +0800

    file.js --> can read the jade's content(summary)

--- file.js
+++ file.js
@@ -4,14 +4,40 @@
 let fs = require("fs");
 let path = require("path");
 
-let dir = process.cwd();
+let dir = "./articles/articles_in_markdown"
+// let text= fs.readFile(dir)
+// console.log(text)
+
 var files = fs.readdirSync(dir);
 files.forEach(function (filename) {
   var fullname = path.join(dir,filename);
   var stats = fs.statSync(fullname);
-  if (stats.isDirectory()) filename += '/';
-  process.stdout.write(filename + '\t' +
-    stats.size + '\t' +
-    stats.mtime + '\n'
-  );
+  //filename += '/';
+  // process.stdout.write(filename + '\t' +
+  //   stats.size + '\t' +
+  //   stats.mtime + '\n'
+  // );
+
+  if (!stats.isDirectory()){
+    process.stdout.write("准备读取....." + filename +"     路径在"+fullname+"\n")
+    fs.readFile(fullname,(error,buffer)=>{
+      if(error){
+        console.log(error)
+      }
+      // console.log("--------------------------  "+filename+"  --------------------------------")
+      // console.log(buffer.toString())
+      // console.log("----------------------------------------------------------")
+
+      // extract the hide-summary
+      let str = buffer.toString();
+      var patt = new RegExp(/<hide-summary>(.*)<\/hide-summary>/,"g");
+      let matchSummary = patt.exec(str);
+      if(!matchSummary){
+        console.warn(filename+ '    没有发现summary');
+        return
+      }
+      console.log("summary", matchSummary[1])
+    })
+
+  }
 });
